338a5cadd2b776542f3694fbcaa101076abd6eaf
chops <charles+oopsie@redguava.com.au>
2017-11-06 11:48:06 +1100

    first commit

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -33,10 +33,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 14
 #define MATRIX_COLS 6
 
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_INTERVAL       30
+#define MOUSEKEY_DELAY          10
+#define MOUSEKEY_TIME_TO_MAX    20
+#define MOUSEKEY_MAX_SPEED      25
 #define MOUSEKEY_WHEEL_DELAY 0
 
 #define TAPPING_TOGGLE  1
